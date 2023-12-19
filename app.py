157d9abe7357534ad900d6f2709d827c004721f6
Adi Vrskic <adi_vrskic1@homedepot.com>
2023-12-19 17:45:16 -0500

    be complete, get data complete

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 import pypandoc
 import os
+import re
+from random import sample, choices
+import json
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import math
 
 app = Flask(__name__)
 
@@ -20,8 +30,64 @@ def upload():
         return ' '.join(map(str, lines)), 200
     else:
         return 'Invalid file type', 400
+    
+@app.route("/scan", methods=["POST"])
+def scan():
+    selected_phrases = extract_phrases()
+    
+    res = []
+    return 'Nice', 200
+    # Make a Google search with each phrase
+    for phrase in selected_phrases:
+        url = 'https://google.com/search?start=0&q=' + phrase.strip()
+        
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        links = soup.findAll("a")
+        
+        agg_similarity = 0
+        highest_similarity = 0
+        highest_similarity_link = ''
+        counter = 0
+        
+        print(phrase)
+        
+        for link in links:
+            link_href = link.get('href')
+            if "url?q=" in link_href and not "webcache" in link_href and counter < 5:
+                counter+=1
+                response = requests.get(url)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                body = soup.find('body')
+                text = body.get_text()
+                
+                # Convert the texts into TF-IDF vectors
+                vectorizer = TfidfVectorizer()
+                vectors = vectorizer.fit_transform([phrase, text])
 
+                # Calculate the cosine similarity between the vectors
+                similarity = cosine_similarity(vectors)
+                
+                if(similarity[0][1] > highest_similarity):
+                    highest_similarity = similarity[0][1]
+                    highest_similarity_link = link
+                
+                agg_similarity += similarity[0][1]
+            
+        res.append({
+            'similarity': float(agg_similarity / 5),
+            'link': str(highest_similarity_link),
+            'phrase': phrase,
+        })                
 
+    
+    if(len(res) == 0):
+        return 'Empty Result', 500
+    
+    return json.dumps(res), 200
+    
+
+# Helpers 
 def is_valid_file(ext):
     return ext in ALLOWED_FILE_TYPES
 
@@ -34,3 +100,11 @@ def convert_to_txt(file):
         for line in fp.readlines():
             lines.append(line)
     return lines
+
+def extract_phrases(num=5):
+    phrases = []
+    for i in range(num):
+        with open('output.txt'.format(i)) as f:
+            phrases += (re.findall(r".*?[\.\!\?]+", f.read()))
+
+    return set(sample(phrases, k=num))
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,26 @@
+const obj = [
+  {
+      "similarity": 0.4598555386678978,
+      "link": "<a data-ved=\"2ahUKEwjd4OjSvJyDAxVUtYQIHflKD5IQgQN6BAgMEAE\" href=\"/url?q=https://scholar.google.com/scholar%3Fq%3Dassessment,%2Bdata%2Binterpretation,%2Band%2Bmicro%2Band%2Bmacroeconomics.%26hl%3Den%26as_sdt%3D0%26as_vis%3D1%26oi%3Dscholart&amp;sa=U&amp;ved=2ahUKEwjd4OjSvJyDAxVUtYQIHflKD5IQgQN6BAgMEAE&amp;usg=AOvVaw0nHf50w4TMGh7aSR1xPdQa\"><span class=\"deIvCb AP7Wnd\"><span class=\"rQMQod Xb5VRe\">Scholarly articles for assessment, data interpretation, and micro and macroeconomics.</span></span></a>",
+      "phrase": "assessment, data interpretation, and micro and macroeconomics."
+  },
+  {
+      "similarity": 0.18823897055681799,
+      "link": "<a class=\"eZt8xd\" href=\"/url?q=/search%3Fq%3Dbusiness%2Bvalue%2Bby%2Badding%2Bover%2B200%2Bresidential%2Bcustomers.%26sca_esv%3D8a501077993ea0e8%26ie%3DUTF-8%26tbm%3Dshop%26source%3Dlnms&amp;opi=89978449&amp;sa=U&amp;ved=0ahUKEwj3kq3VvJyDAxW3RTABHUypD-cQiaAMCAgoAw&amp;usg=AOvVaw0KCrCDiuvY6KH0SSmPeQuG\">Shopping</a>",
+      "phrase": "      business value by adding over 200 residential customers."
+  },
+  {
+      "similarity": 0.46536115655667654,
+      "link": "<a data-ved=\"2ahUKEwj_9OPXvJyDAxVnRTABHTZvAV4QFnoECAAQAg\" href=\"/url?q=https://www.indeed.com/career-advice/career-development/research-databases&amp;sa=U&amp;ved=2ahUKEwj_9OPXvJyDAxVnRTABHTZvAV4QFnoECAAQAg&amp;usg=AOvVaw2-J0PimCef6p30pb1tsfIE\"><div class=\"DnJfK\"><div class=\"j039Wc\"><h3 class=\"zBAuLc l97dzf\"><div class=\"BNeawe vvjwJb AP7Wnd\">23 Research Databases for Professional and Academic Use - Indeed</div></h3></div><div class=\"sCuL3\"><div class=\"BNeawe UPmit AP7Wnd lRVwie\">www.indeed.com › Career Guide › Career development</div></div></div></a>",
+      "phrase": "      databases for reliable reference."
+  },
+  {
+      "similarity": 0.4982161102183709,
+      "link": "<a href=\"/url?q=/search%3Fq%3D-%2B%2B%2BLed%2Ba%2Bteam%2Bin%2Ba%2Bbusiness%2Bsimulation%2Bproject.%26sca_esv%3D8a501077993ea0e8%26ie%3DUTF-8%26tbm%3Dshop%26source%3Dlnms&amp;opi=89978449&amp;sa=U&amp;ved=0ahUKEwi9zMbavJyDAxV7TTABHV8NBkQQiaAMCAooBQ&amp;usg=AOvVaw3wX9FJR3gj_rDbD74yBBWg\">Shopping</a>",
+      "phrase": "-   Led a team in a business simulation project."
+  }
+];
+
 $(function() {
   $('#custom-file-input').on('change', function() {
       const form_data = new FormData($('#form')[0]);
@@ -9,22 +32,79 @@ $(function() {
           cache: false,
           processData: false,
           beforeSend: function() {
-            console.log("Loading indicator stuff")
+            $('.custom-form-error').removeClass('visible');
+            $('.custom-form-server-error').removeClass('visible');
+            $('.custom-form-loading').addClass('visible');
           },
           success: function(data) {
             $('.custom-form-error').removeClass('visible');
+            $('.custom-form-loading').removeClass('visible');
             $(".textarea").val(data);
           },
           error: function(err) {
             if(err.status === 400) {
               $(".textarea").val('');
+              $('.custom-form-loading').removeClass('visible');
               $('.custom-form-error').addClass('visible');
             }
           }
       });
   });
 
+  $('.scan').on('click', function() {
+    if($('.textarea').val().length < 100) {
+      $('.notification').addClass('visible');
+    } else {
+      $('.notification').removeClass('visisble');
+      $.ajax({
+        type: 'POST',
+        url: '/scan',
+        contentType: false,
+        cache: false,
+        processData: false,
+        beforeSend: function() {
+          $('.custom-form-server-error').removeClass('visible');
+          $('.custom-form-loading').addClass('visible');
+        },
+        success: function(data) {
+          $('.custom-form-error').removeClass('visible');
+          $('.custom-form-loading').removeClass('visible');
+          
+          $('.custom-results').addClass('has-results');
+
+          appendResults(obj);
+          
+        },
+        error: function(err) {
+          if(err.status === 500) {
+            $('.custom-form-loading').removeClass('visible');
+            $('.custom-form-server-error').addClass('visible');
+          }
+        }
+      });
+    }
+  })
+
   $('.custom-form-error-close').on('click', function() {
     $('.custom-form-error').removeClass('visible');
+    $('.custom-form-server-error').removeClass('visible');
   })
+
+  $('.delete').on('click', function() {
+    $('.notification').removeClass('visible');
+  })
+
+  const appendResults = data => {
+    data.map(item => {
+      const el = $("<div></div>").html(`
+        <div class="custom-result">
+          <h3 class="custom-result-phrase">${item.phrase}</h3>
+          ${item.link}
+          <span class="custom-result-percentage">${item.similarity * 100}</span>
+        </div>
+      `);
+      $(".custom-results-container").append(el);
+    })
+  }
 });
+
